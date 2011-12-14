c79731de28bb99c70b4012000c1b5cf1193fb89d
Nils Werner <nils.werner@gmail.com>
2011-12-14 12:26:34 +0100

    Nochmal 2x so viele Sinusse

--- main.m
+++ main.m
@@ -13,15 +13,15 @@ frame_overlap = 10; % ms
 frame_length  = 20;
 window        = 'hamming';
 
-nfft = round(frame_length  * sf / 100); % convert ms to points
-noverlap = round(frame_overlap * sf / 100); % convert ms to points
+nfft = round(frame_length  * sf / 50); % convert ms to points
+noverlap = round(frame_overlap * sf / 50); % convert ms to points
 window   = eval(sprintf('%s(nfft)', window)); % e.g., hamming(nfft)
 
 [S,F,T,P] = spectrogram(flsig(:,1), window, noverlap, nfft, sf);
 surf(T,F,10*log10(P),'edgecolor','none'); axis tight;
 
 % 75te Spalte = Spektrum zum Zeitpunkt 0:00:00:75
-sspek = P(:,7)';
+sspek = P(:,3)';
 
 % Sinus
 %sf = 44100;
