e2969261374c6379ebc5b20c34b02304f2fc9ce5
Nils Werner <nils.werner+lms@gmail.com>
2012-01-12 12:22:56 +0100

    Spectrogramm-Code entfernt

--- main.m
+++ main.m
@@ -22,26 +22,6 @@ ylabel('Power (dB)')
 
 
 %
-% Kurzzeit-Spektrum der Floete
-%
-
-disp('Analyzing Spectrum')
-frame_overlap = 10; % ms
-frame_length  = 20;
-window        = 'hamming';
-
-nfft = round(frame_length  * sf / 50); % convert ms to points
-noverlap = round(frame_overlap * sf / 50); % convert ms to points
-window   = eval(sprintf('%s(nfft)', window)); % e.g., hamming(nfft)
-
-[S,F,T,P] = spectrogram(flsig(:,1), window, noverlap, nfft, sf);
-surf(T,F,10*log10(P),'edgecolor','none'); axis tight;
-
-
-
-
-
-%
 % Spektrum extrahieren und normalisieren
 %
 
