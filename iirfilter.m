44647db4521e332e729dd0539ec17df24080fad2
Nils Werner <nils.werner@gmail.com>
2012-02-04 17:56:32 +0100

    Nervige imaginary part warnung weg

--- iirfilter.m
+++ iirfilter.m
@@ -110,7 +110,7 @@ hold off
 if plotting == 1
 	figure(freqs);
 	for i = m;
-		plot(ws(:,i)',20*log10(as(i)*abs(hs(:,i)')),'color',cc(i,:));
+		plot(ws(:,i)',20*log10(abs(as(i))*abs(hs(:,i)')),'color',cc(i,:));
 		hold on
 	end
 	axis([0 11000 70 180]);
